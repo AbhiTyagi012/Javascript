6491f5d8e227e39b42283394cf1239e36d6e4046
AbhiTyagi012 <99748179+AbhiTyagi012@users.noreply.github.com>
2025-05-24 03:20:15 +0000

    - variables,datatypes and conversion

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,26 @@
+const accountId = 132453
+let accountEmail = "abhi@gmail.com"
+var accountPassword = "12345"
+accountCity = "Gurugram"
+let accountState
+
+// console.log(accountId);
+
+accountEmail = "test@gmail.com"
+accountPassword = "67890"
+accountCity = "Delhi"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
+//---------------
+// console.table([]) -> to print data in table
+
+
+
+
+
+/*
+Prefer to use let instead of var 
+due to block scope and functional scope
+*/
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1; false =>0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = undefined
+
+let boolenIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolenIsLoggedIn)
+
+// 1 => true; 0=> false
+// "" => false
+// "abhi" => true
+// null => flase; undefined => false
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof(stringNumber))
