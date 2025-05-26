818e377aa369f709e4b27de439901976743aafbb
AbhiTyagi012 <99748179+AbhiTyagi012@users.noreply.github.com>
2025-05-26 11:08:16 +0000

    - strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,39 @@
+const name = "Abhi"
+const count = 50
+
+// console.log(name + count)
+// console.log(` Hello my Name is ${name} and my repo count is ${count}`)
+
+const gameName = new String('new-game')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('n'));
+
+// const newString = gameName.substring(0,3)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-7,4)
+// console.log(anotherString)
+
+const newStringOne = "      abhi     "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+const url = "http://localhost:5000/abhi%20tyagi"
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('new'))
+
+console.log(gameName.split('-'))
+
+
+
+
+
