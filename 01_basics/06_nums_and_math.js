377a268fe0fea852a34201a9443ac875da38d73f
AbhiTyagi012 <99748179+AbhiTyagi012@users.noreply.github.com>
2025-05-29 11:35:38 +0000

    - numbers and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,35 @@
+const score = 400
+// console.log(score);
+
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8978
+
+// console.log(otherNumber.toPrecision(4))
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+// ***************** Maths *************** //
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(5.6))
+// console.log(Math.ceil(4.2))
+
+// console.log(Math.min(4,2,1,3));
+// console.log(Math.max(4,2,1,3));
+
+console.log(Math.random()) // always between 1 & 0
+console.log((Math.random()* 10) + 1)
+console.log(Math.floor(Math.random()* 10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
